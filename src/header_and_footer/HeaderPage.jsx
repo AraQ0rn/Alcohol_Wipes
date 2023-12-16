6b1261bbfd91db6ef3b3f920c0f9c3dde94af812
daniil <test@test.com>
2023-12-16 15:59:32 +0300

    header

--- src/header_and_footer/HeaderPage.jsx
+++ src/header_and_footer/HeaderPage.jsx
@@ -3,16 +3,35 @@ import style from './headerPage.module.css'
 
 export const HeaderPage = () => {
 
-    return (<>
-        <div>
-            <NavLink to='/'>Главная</NavLink>
-            <NavLink to='catalog'>Каталог</NavLink>
-            <NavLink to='delivery_and_payment' >Доставка и оплата</NavLink>
-            <NavLink to='wholesale'>Опт</NavLink>
-            <NavLink to='contacts'>Контакты</NavLink>
-            <NavLink to='basket'>Корзина</NavLink>
-            <a>+7(888)888-88-88</a>
+    const active = (ev) => ev.isActive ? style.active : style.link
+
+    return (
+        <div className={style.container}>
+            <div className={style.header}>
+                <div className={style.links}>
+                    <div>
+                        <NavLink className={active} to='/'>Главная</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={active} to='catalog'>Каталог</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={active} to='delivery_and_payment' >Доставка и оплата</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={active} to='wholesale'>Опт</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={active} to='contacts'>Контакты</NavLink>
+                    </div>
+                    <div>
+                        <NavLink className={active} to='basket'>Корзина</NavLink>
+                    </div>
+                </div>
+                <div>
+                    <a className={style.tel} href="tel: +78888888888">+7(888)888-88-88</a>
+                </div>
+            </div>
         </div>
-    </>
     )
 }
